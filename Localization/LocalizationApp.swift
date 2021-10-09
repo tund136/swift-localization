4869ed33967943bc2a08292b7afe5ae222d547c0
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-09 08:58:04 +0700

    Create the project

---
+++ Localization/LocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationApp.swift
+//  Localization
+//
+//  Created by Danh Tu on 09/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
